936f253d0441d50fd07aa40c71cd37799f73206b
Ric Allinson <allinson@yahoo-inc.com>
2016-10-15 16:35:05 -0700

    Cleanup

--- src/github.com/ricallinson/testhello/hello.c
+++ src/github.com/ricallinson/testhello/hello.c
@@ -1,13 +1,15 @@
 // Entry Point.
 
 #include <stdio.h>
+#include "github.com/ricallinson/testlib/math.h"
 #include "github.com/ricallinson/teststruct/account.h"
 
 int main() {
     Account a = AccountCreate("James", "Jones");
+    MathNext();
     Account b = AccountCreate("Rob", "Red");
     char *s;
-    AccountToString(s, &b);
+    AccountToString(&b, s);
     printf("%s\n", s);
     return 0;
 }
