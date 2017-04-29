39d8ef4733c64fcf65afe909fd5f85ff99c2359a
Ric Allinson <ric@ricallinson.com>
2017-04-29 11:41:09 -0700

    Started on Clean

--- workspace/src/github.com/cmm/path/main.c
+++ workspace/src/github.com/cmm/path/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *subset(char *array, int from, int to) {
+char *copySubset(char *array, int from, int to) {
 	int arraySize = strlen(array);
 	int n = 0;
 	char *res = malloc((to-from)*sizeof(char));
@@ -13,38 +13,75 @@ char *subset(char *array, int from, int to) {
 	return res;
 }
 
+// Base returns the last element of path. Trailing slashes are removed before extracting the last element. 
+// If the path is empty, Base returns ".". If the path consists entirely of slashes, Base returns "/".
 char *PathBase(char *path) {
+	// If the path is empty return the relative current directory.
 	if (strcmp(path, "") == 0) {
-		return ".";
+		char *res = malloc(sizeof(char)*2);
+		res[0] = '.';
+		res[1] = 0;
+		return res;
 	}
 	// Copy the path into a new array so it can be changed without side effects.
-	char *res = subset(path, 0, strlen(path));
+	char *res = copySubset(path, 0, strlen(path));
 	// Strip trailing slashes.
 	while (strlen(res) > 0 && res[strlen(res)-1] == '/') {
 		// Create a new array with the shortened path.
-		char* tmp = subset(res, 0, strlen(res)-1);
+		char* tmp = copySubset(res, 0, strlen(res)-1);
 		// Free the old result value memory now we have the new array tmp.
 		free(res);
 		// Copy tmp into the return value pointer.
-		res = subset(tmp, 0, strlen(tmp));
+		res = copySubset(tmp, 0, strlen(tmp));
 		// Now free the tmp value memory as we don't need it anymore.
 		free(tmp);
 	}
 	// Find the last element
 	for (int i = strlen(res); i > 0; i--) {
 		if (res[i] == '/') {
-			res = subset(res, 0, i);
+			res = copySubset(res, 0, i);
 			break;
 		}
 	}
 	// If empty now, it had only slashes.
 	if (strcmp(res, "") == 0) {
-		return "/";
+		free(res);
+		char *res = malloc(sizeof(char)*2);
+		res[0] = '/';
+		res[1] = 0;
+		return res;
 	}
 	return res;
 }
 
+// Clean returns the shortest path name equivalent to path
+// by purely lexical processing. It applies the following rules
+// iteratively until no further processing can be done:
+//
+//	1. Replace multiple slashes with a single slash.
+//	2. Eliminate each . path name element (the current directory).
+//	3. Eliminate each inner .. path name element (the parent directory)
+//	   along with the non-.. element that precedes it.
+//	4. Eliminate .. elements that begin a rooted path:
+//	   that is, replace "/.." by "/" at the beginning of a path.
+//
+// The returned path ends in a slash only if it is the root "/".
+//
+// If the result of this process is an empty string, Clean
+// returns the string ".".
+//
+// See also Rob Pike, ``Lexical File Names in Plan 9 or
+// Getting Dot-Dot Right,''
+// https://9p.io/sys/doc/lexnames.html
 char *PathClean(char *path) {
+	// If the path is empty return the relative current directory.
+	if (strcmp(path, "") == 0) {
+		char *res = malloc(sizeof(char)*2);
+		res[0] = '.';
+		res[1] = 0;
+		return res;
+	}
+	int rooted = path[0] == '/';
 	return "";
 }
 
--- workspace/src/github.com/cmm/path/main_test.c
+++ workspace/src/github.com/cmm/path/main_test.c
@@ -7,33 +7,47 @@
 void TestPathBaseEmpty() {
 	char *path = PathBase("");
 	assert(strcmp(path, ".") == 0);
+	free(path);
 }
 
 void TestPathBaseRootPath() {
 	char *path = PathBase("/foo/bar/baz");
 	assert(strcmp(path, "/foo/bar") == 0);
+	free(path);
 }
 
 void TestPathBaseRelativePath() {
 	char *path = PathBase("./bar/baz");
 	assert(strcmp(path, "./bar") == 0);
+	free(path);
 }
 
 void TestPathBaseRoot() {
 	char *path = PathBase("/");
 	assert(strcmp(path, "/") == 0);
+	free(path);
 }
 
 void TestPathBaseRootPathBroken() {
 	char *path = PathBase("/foo//bar//baz");
 	assert(strcmp(path, "/foo//bar/") == 0);
+	free(path);
+}
+
+void TestPathCleanEmpty() {
+	char *path = PathClean("");
+	assert(strcmp(path, ".") == 0);
+	free(path);
 }
 
 int main() {
+	// Base.
 	TestPathBaseEmpty();
 	TestPathBaseRootPath();
 	TestPathBaseRelativePath();
 	TestPathBaseRoot();
 	TestPathBaseRootPathBroken();
+	// Clean.
+	TestPathCleanEmpty();
 	return 0;
 }
